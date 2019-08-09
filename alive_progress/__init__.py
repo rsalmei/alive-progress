fd74202410c9b07d276f0f1aa20fb3381cfe0d94
Rogério <rsalmei@gmail.com>
2019-08-09 01:20:46 -0300

    bump version

--- alive_progress/__init__.py
+++ alive_progress/__init__.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-VERSION = (1, 0)
+VERSION = (1, 0, 1)
 
 __author__ = 'Rogério Sampaio de Almeida'
 __email__ = 'rsalmei@gmail.com'
