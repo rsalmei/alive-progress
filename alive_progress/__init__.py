15b1dc06b104fceea0b8687275535509832db691
Rogério <rsalmei@gmail.com>
2019-08-09 01:38:27 -0300

    support 'from alive_progress import *' again in python 2.7

--- alive_progress/__init__.py
+++ alive_progress/__init__.py
@@ -20,3 +20,8 @@ __all__ = ['alive_bar', 'standard_bar_factory', 'unknown_bar_factory', 'spinner_
            'frame_spinner_factory', 'scrolling_spinner_factory', 'bouncing_spinner_factory',
            'compound_spinner_factory', 'delayed_spinner_factory', 'BARS', 'SPINNERS',
            'showtime', 'config_handler']
+
+import sys
+
+if sys.version_info <= (3,):
+    __all__ = [bytes(x) for x in __all__]
