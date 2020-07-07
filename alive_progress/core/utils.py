5ab67e7a8e90fc1cc029605d3143f41095fa4ce2
Rogério Sampaio <rsalmei@gmail.com>
2020-07-07 00:11:45 -0300

    terminal_columns support, including python 2

--- alive_progress/core/utils.py
+++ alive_progress/core/utils.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import os
 import sys
 
 
@@ -21,3 +22,39 @@ def show_cursor():
 
 def sanitize_text(text):
     return ' '.join(str(text).splitlines())
+
+
+def terminal_columns():  # pragma: no cover
+    """Gets the size of the terminal.
+
+    This should work only on *nix, macOS included.
+    Heavily based on console.py in https://stackoverflow.com/a/566752/1296256
+    # TODO use shutil on python 3.
+
+    At least this seems extremely fast, so no problem calling it in all refreshes:
+    In [3]: %timeit terminal_columns()
+    2.39 µs ± 28.7 ns per loop (mean ± std. dev. of 7 runs, 100000 loops each)
+
+    Returns:
+        int: number of columns in the current interactive terminal.
+
+    """
+
+    def ioctl_GWINSZ(fd):
+        try:
+            import fcntl, termios, struct
+            return struct.unpack('hh', fcntl.ioctl(fd, termios.TIOCGWINSZ, '1234'))  # noqa
+        except:
+            pass
+
+    cr = ioctl_GWINSZ(0) or ioctl_GWINSZ(1) or ioctl_GWINSZ(2)
+    if not cr:
+        try:
+            fd = os.open(os.ctermid(), os.O_RDONLY)
+            cr = ioctl_GWINSZ(fd)
+            os.close(fd)
+        except:
+            pass
+    if not cr:
+        cr = None, os.getenv('COLUMNS', 80)
+    return int(cr[1])
