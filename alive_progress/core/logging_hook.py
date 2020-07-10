a89d1695178eb62e64390d96e13f55f2e2293c51
Rogério Sampaio <rsalmei@gmail.com>
2020-07-10 13:47:26 -0300

    fix missing StreamHandler method in versions prior than 3.7

--- alive_progress/core/logging_hook.py
+++ alive_progress/core/logging_hook.py
@@ -4,9 +4,25 @@ import sys
 
 def install_logging_hook():  # pragma: no cover
     root = logging.root
-    return {h: h.setStream(sys.stdout) for h in root.handlers
             if isinstance(h, logging.StreamHandler)}
+    return {h: set_stream(h, sys.stdout) for h in root.handlers
 
 
 def uninstall_logging_hook(before):  # pragma: no cover
-    [h.setStream(s) for h, s in before.items()]
+    [set_stream(h, s) for h, s in before.items()]
+
+
+if sys.version_info >= (3, 7):
+    def set_stream(handler, stream):
+        return handler.setStream(stream)
+else:
+    def set_stream(handler, stream):
+        # from python 3.7 implementation.
+        result = handler.stream
+        handler.acquire()
+        try:
+            handler.flush()
+            handler.stream = stream
+        finally:
+            handler.release()
+        return result
