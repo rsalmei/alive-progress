e86196fa4ed37b961d006025f8a1415ae13c182f
Rogério Sampaio <rsalmei@gmail.com>
2020-08-04 16:56:42 -0300

    new tests for logging support

--- tests/core/test_logging_hook.py
+++ tests/core/test_logging_hook.py
@@ -1,29 +1,37 @@
 import logging
 import sys
+from unittest import mock
 
 import pytest
 
-from alive_progress.core.logging_hook import install_logging_hook, uninstall_logging_hook
+from alive_progress.core.logging_hook import install_logging_hooks, uninstall_logging_hooks
 
 
 @pytest.fixture
-def all_handlers():
-    handler = logging.StreamHandler(sys.stderr)
-    nope = logging.FileHandler('/dev/null', delay=True)
-    [logging.root.addHandler(h) for h in (handler, nope)]
-    yield handler, nope
-    [logging.root.removeHandler(h) for h in (handler, nope)]
+def handlers():
+    handlers = (logging.StreamHandler(sys.stderr),
+                logging.StreamHandler(sys.stdout),
+                logging.FileHandler('/dev/null', delay=True))
+    [logging.root.addHandler(h) for h in handlers]
+    yield {handler: handler.stream for handler in handlers}
+    [logging.root.removeHandler(h) for h in handlers]
 
 
-def test_install(all_handlers):
-    handler, nope = all_handlers
-    changed = install_logging_hook()
-    assert handler.stream == sys.stdout
-    assert nope.stream is None
-    assert changed == {handler: sys.stderr}
+def test_install(handlers):
+    hook_manager = mock.Mock()
+    hook_manager.get_hook_for.side_effect = lambda s: s
+    with mock.patch('alive_progress.core.logging_hook.set_stream') as mock_set_stream:
+        mock_set_stream.side_effect = lambda h, s: s
+        changed = install_logging_hooks(hook_manager)
+
+    assert handlers == {k: v for k, v in changed.items() if k in handlers}  # pytest has one.
+    hook_manager.get_hook_for.assert_has_calls(tuple(mock.call(x) for x in handlers.values()),
+                                               any_order=True)
+    mock_set_stream.assert_has_calls(tuple(mock.call(h, s) for h, s in handlers.items()),
+                                     any_order=True)
 
 
-def test_uninstall(all_handlers):
-    handler, _ = all_handlers
-    uninstall_logging_hook({handler: sys.stderr})
-    assert handler.stream == sys.stderr
+def test_uninstall(handlers):
+    with mock.patch('alive_progress.core.logging_hook.set_stream') as mock_set_stream:
+        uninstall_logging_hooks(handlers)
+    mock_set_stream.assert_has_calls(mock.call(h, s) for h, s in handlers.items())
