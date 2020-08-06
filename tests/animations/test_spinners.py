a2ab97c77542b8be76b5e92ce57b0b42bbbaf769
Rogério Sampaio <rsalmei@gmail.com>
2020-08-06 00:25:45 -0300

    test all the new spinners implementations

--- tests/animations/test_spinners.py
+++ tests/animations/test_spinners.py
@@ -21,9 +21,17 @@ def test_frame_spinner(frames, expected):
     (None, None, ' ', True, False, (('abc', 'cab', 'bca'),)),
     (None, None, ' ', False, False, (('abc', 'bca', 'cab'),)),
     (2, None, '~', True, True, (('~~', 'c~', 'bc', 'ab', '~a'),)),
+    (2, None, '~', True, False, (('bc', 'ab', 'ca'),)),
     (2, None, '~', False, True, (('~~', '~a', 'ab', 'bc', 'c~'),)),
+    (2, None, '~', False, False, (('ab', 'bc', 'ca'),)),
+    (3, None, '~', True, True, (('~~~', 'c~~', 'bc~', 'abc', '~ab', '~~a'),)),
+    (3, None, '~', True, False, (('abc', 'cab', 'bca'),)),
+    (3, None, '~', False, True, (('~~~', '~~a', '~ab', 'abc', 'bc~', 'c~~'),)),
+    (3, None, '~', False, False, (('abc', 'bca', 'cab'),)),
     (4, None, ' ', True, True, (('    ', 'c   ', 'bc  ', 'abc ', ' abc', '  ab', '   a'),)),
+    (4, None, ' ', True, False, (('abc ', ' abc', 'c ab', 'bc a'),)),
     (4, None, ' ', False, True, (('    ', '   a', '  ab', ' abc', 'abc ', 'bc  ', 'c   '),)),
+    (4, None, ' ', False, False, ((' abc', 'abc ', 'bc a', 'c ab'),)),
     (4, 1, '_', True, True, (('____', 'a___', '_a__', '__a_', '___a'),
                              ('____', 'b___', '_b__', '__b_', '___b'),
                              ('____', 'c___', '_c__', '__c_', '___c'))),
@@ -34,39 +42,120 @@ def test_frame_spinner(frames, expected):
 def test_scrolling_spinner(length, block, blank, right, hiding, expected):
     spinner_factory = scrolling_spinner_factory('abc', length, block, blank, right, hiding)
     spinner = spinner_factory(length_actual=None)  # natural spinner size.
-    for result in expected:
-        cycle = spinner()
-        assert tuple(cycle) == result
+    assert tuple(tuple(spinner()) for _ in expected) == expected
 
 
 @pytest.mark.parametrize('length, block, blank, hiding, expected', [
-    (3, None, ' ', True, (('   ', 'c  ', 'bc ', 'abc', ' ab', '  a',
-                           '   ', '  d', ' de', 'def', 'ef ', 'f  '),)),
+    (None, None, None, True, (('   ', 'c  ', 'bc ', 'abc', ' ab', '  a',
+                               '   ', '  d', ' de', 'def', 'ef ', 'f  '),)),
+    (None, None, None, False, (('abc', 'def'),)),
     (2, None, '~', True, (('~~', 'c~', 'bc', 'ab', '~a', '~~', '~d', 'de', 'ef', 'f~'),)),
-    (3, None, ' ', False, (('abc', 'def'),)),
-    (6, None, ' ', False, (('abc   ', ' abc  ', '  abc ', '   def', '  def ', ' def  '),)),
+    (2, None, '~', False, (('bc', 'ab', 'de', 'ef'),)),
+    (3, None, '+', True, (('+++', 'c++', 'bc+', 'abc', '+ab', '++a',
+                           '+++', '++d', '+de', 'def', 'ef+', 'f++'),)),
+    (3, None, '+', False, (('abc', 'def'),)),
+    (4, None, ' ', True, (('    ', 'c   ', 'bc  ', 'abc ', ' abc', '  ab', '   a',
+                           '    ', '   d', '  de', ' def', 'def ', 'ef  ', 'f   '),)),
+    (4, None, ' ', False, (('abc ', ' abc', ' def', 'def '),)),
     (3, 1, '_', True, (('___', 'a__', '_a_', '__a', '___', '__d', '_d_', 'd__'),
                        ('___', 'b__', '_b_', '__b', '___', '__e', '_e_', 'e__'),
                        ('___', 'c__', '_c_', '__c', '___', '__f', '_f_', 'f__'))),
-    (5, 2, '_', False, (('aa___', '_aa__', '__aa_', '___dd', '__dd_', '_dd__'),
-                        ('bb___', '_bb__', '__bb_', '___ee', '__ee_', '_ee__'),
-                        ('cc___', '_cc__', '__cc_', '___ff', '__ff_', '_ff__'))),
+    (5, 2, '_', False, (('aa___', '_aa__', '__aa_', '___aa', '___dd', '__dd_', '_dd__', 'dd___'),
+                        ('bb___', '_bb__', '__bb_', '___bb', '___ee', '__ee_', '_ee__', 'ee___'),
+                        ('cc___', '_cc__', '__cc_', '___cc', '___ff', '__ff_', '_ff__', 'ff___'))),
 ])
-def test_bouncing_spinner(length, block, blank, hiding, expected):
-    spinner_factory = bouncing_spinner_factory('abc', length, block, 'def', blank, hiding)
+def test_bouncing_spinner_no_text(length, block, blank, hiding, expected):
+    spinner_factory = bouncing_spinner_factory(('abc', 'def'), length, block, blank, hiding)
     spinner = spinner_factory(length_actual=None)  # natural spinner size.
-    for result in expected:
-        cycle = spinner()
-        assert tuple(cycle) == result
+    assert tuple(tuple(spinner()) for _ in expected) == expected
+
+
+@pytest.mark.parametrize('length, block, blank, hiding, expected', [
+    (None, None, None, True, (
+            ('   ', '   ', '   ', '   ', '   ', '   ', '  a', '  a', ' ab', ' ab', 'abc', 'abc',
+             'bc ', 'bc ', 'c  ', 'c  ', '   ', '   ', '   ', '   ', '   ', '   ', 'f  ', 'f  ',
+             'ef ', 'ef ', 'def', 'def', ' de', ' de', '  d', '  d'),
+    )),
+    (None, None, None, False, (
+            ('abc', 'abc', 'abc', 'abc', 'abc', 'abc', 'def', 'def', 'def', 'def', 'def', 'def'),
+    )),
+    (2, None, '~', True, (
+            ('~~', '~~', '~~', '~~', '~~', '~~', '~a', '~a', 'ab', 'ab', 'bc', 'bc', 'c~', 'c~',
+             '~~', '~~', '~~', '~~', '~~', '~~', 'f~', 'f~', 'ef', 'ef', 'de', 'de', '~d', '~d'),
+    )),
+    (2, None, '~', False, (
+            ('ab', 'ab', 'ab', 'ab', 'ab', 'ab', 'bc', 'bc', 'bc', 'bc', 'bc', 'bc', 'ef', 'ef',
+             'ef', 'ef', 'ef', 'ef', 'de', 'de', 'de', 'de', 'de', 'de'),
+    )),
+    (3, None, '+', True, (
+            ('+++', '+++', '+++', '+++', '+++', '+++', '++a', '++a', '+ab', '+ab', 'abc', 'abc',
+             'bc+', 'bc+', 'c++', 'c++', '+++', '+++', '+++', '+++', '+++', '+++', 'f++', 'f++',
+             'ef+', 'ef+', 'def', 'def', '+de', '+de', '++d', '++d'),
+    )),
+    (3, None, ' ', False, (
+            ('abc', 'abc', 'abc', 'abc', 'abc', 'abc', 'def', 'def', 'def', 'def', 'def', 'def'),
+    )),
+    (4, None, ' ', True, (
+            ('    ', '    ', '    ', '    ', '    ', '    ', '   a', '   a', '  ab', '  ab', ' abc',
+             ' abc', 'abc ', 'abc ', 'bc  ', 'bc  ', 'c   ', 'c   ', '    ', '    ', '    ', '    ',
+             '    ', '    ', 'f   ', 'f   ', 'ef  ', 'ef  ', 'def ', 'def ', ' def', ' def', '  de',
+             '  de', '   d', '   d'),
+    )),
+    (4, None, ' ', False, (
+            (' abc', ' abc', ' abc', ' abc', ' abc', ' abc', 'abc ', 'abc ', 'abc ', 'abc ', 'abc ',
+             'abc ', 'def ', 'def ', 'def ', 'def ', 'def ', 'def ', ' def', ' def', ' def', ' def',
+             ' def', ' def'),
+    )),
+    (3, 1, '_', True, (
+            ('___', '___', '___', '___', '___', '___', '__a', '__a', '_a_', '_a_', 'a__', 'a__',
+             '___', '___', '___', '___', '___', '___', 'd__', 'd__', '_d_', '_d_', '__d', '__d'),
+            ('___', '___', '___', '___', '___', '___', '__b', '__b', '_b_', '_b_', 'b__', 'b__',
+             '___', '___', '___', '___', '___', '___', 'e__', 'e__', '_e_', '_e_', '__e', '__e'),
+            ('___', '___', '___', '___', '___', '___', '__c', '__c', '_c_', '_c_', 'c__', 'c__',
+             '___', '___', '___', '___', '___', '___', 'f__', 'f__', '_f_', '_f_', '__f', '__f')
+    )),
+    (5, 2, '_', False, (
+            ('___aa', '___aa', '___aa', '___aa', '___aa', '___aa', '__aa_', '__aa_', '_aa__',
+             '_aa__', 'aa___', 'aa___', 'aa___', 'aa___', 'aa___', 'aa___', 'dd___', 'dd___',
+             'dd___', 'dd___', 'dd___', 'dd___', '_dd__', '_dd__', '__dd_', '__dd_', '___dd',
+             '___dd', '___dd', '___dd', '___dd', '___dd'),
+            ('___bb', '___bb', '___bb', '___bb', '___bb', '___bb', '__bb_', '__bb_', '_bb__',
+             '_bb__', 'bb___', 'bb___', 'bb___', 'bb___', 'bb___', 'bb___', 'ee___', 'ee___',
+             'ee___', 'ee___', 'ee___', 'ee___', '_ee__', '_ee__', '__ee_', '__ee_', '___ee',
+             '___ee', '___ee', '___ee', '___ee', '___ee'),
+            ('___cc', '___cc', '___cc', '___cc', '___cc', '___cc', '__cc_', '__cc_', '_cc__',
+             '_cc__', 'cc___', 'cc___', 'cc___', 'cc___', 'cc___', 'cc___', 'ff___', 'ff___',
+             'ff___', 'ff___', 'ff___', 'ff___', '_ff__', '_ff__', '__ff_', '__ff_', '___ff',
+             '___ff', '___ff', '___ff', '___ff', '___ff')
+    )),
+])
+def test_bouncing_spinner_is_text(length, block, blank, hiding, expected):
+    spinner_factory = bouncing_spinner_factory(('abc', 'def'), length, block, blank, hiding,
+                                               is_text=True)
+    spinner = spinner_factory(length_actual=None)  # natural spinner size.
+    assert tuple(tuple(spinner()) for _ in expected) == expected
+
+
+@pytest.mark.parametrize('length_actual, inputs, expected', [
+    (None, ('123', 'abc'), ('1a', '2b', '3c')),
+    (None, ('12345', 'abc'), ('1a', '2b', '3c', '4a', '5b')),
+    (None, (('123', '456'), 'abc'), ('123a', '456b', '123c')),
+    (4, ('123', 'abc'), ('11aa', '22bb', '33cc')),
+])
+def test_compound_spinner_alongside(length_actual, inputs, expected, spinner_test):
+    spinner_factory = compound_spinner_factory(*(spinner_test(x) for x in inputs), alongside=True)
+    spinner = spinner_factory(length_actual=length_actual)
+    cycle = spinner()
+    assert tuple(cycle) == expected
 
 
 @pytest.mark.parametrize('outputs, expected', [
-    (('123', 'abc'), ('1a', '2b', '3c')),
-    (('12345', 'abc'), ('1a', '2b', '3c', '4a', '5b')),
-    ((('123', '456'), 'abc'), ('123a', '456b', '123c')),
+    (('123', 'abc'), ('1', '2', '3', 'a', 'b', 'c')),
+    (('12345', 'abc'), ('1', '2', '3', '4', '5', 'a', 'b', 'c')),
+    ((('123', '456'), 'abc'), ('123', '456', 'aaa', 'bbb', 'ccc')),
 ])
-def test_compound_spinner(outputs, expected, spinner_test):
-    spinner_factory = compound_spinner_factory(*(spinner_test(x) for x in outputs))
+def test_compound_spinner_sequential(outputs, expected, spinner_test):
+    spinner_factory = compound_spinner_factory(*(spinner_test(x) for x in outputs), alongside=False)
     spinner = spinner_factory(length_actual=None)  # natural spinner size.
     cycle = spinner()
     assert tuple(cycle) == expected
