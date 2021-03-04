bef36720fa7fb8635f573e0281f4dec5823d62ef
Rogério Sampaio <rsalmei@gmail.com>
2021-03-03 23:57:40 -0300

    organize code

--- demo.py
+++ demo.py
@@ -1,5 +1,8 @@
 import time
 
+from alive_progress import alive_bar
+from alive_progress.tools.sampling import OVERHEAD_SAMPLING
+
 cases = [
     (5000, dict(total=5000, manual=False, title='Normal auto')),
     (4000, dict(total=6000, manual=False, title='Underflow auto')),
@@ -13,7 +16,6 @@ cases = [
     # (5000, dict(total=..., manual=True, title='Calculating manual')),
 ]
 
-from alive_progress import alive_bar
 for total, case in cases:
     with alive_bar(title_length=16, **case) as bar:
         # bar.text('Quantifying...')
@@ -29,10 +31,9 @@ for total, case in cases:
             for i in range(1, total + 1):
                 time.sleep(.0005)
                 bar()
-            # if i and i%3800==0: print('nice')
+                # if i and i%3800==0: print('nice')
 print()
 
-from alive_progress.core.progress import OVERHEAD_SAMPLING
 for name, config in OVERHEAD_SAMPLING:
     with alive_bar(5000, title_length=16, title=name, **config) as bar:
         # bar.text('Quantifying...')
