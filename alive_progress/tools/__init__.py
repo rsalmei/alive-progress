c2a85a6501d37831ac7da701189dd16407f84d24
Rogério Sampaio <rsalmei@gmail.com>
2021-04-13 20:09:52 -0300

    fix python module warning on validate_unicode_breaks

--- alive_progress/tools/__init__.py
+++ alive_progress/tools/__init__.py
@@ -1,5 +1,4 @@
 from .general import print_chars
 from .sampling import overhead, overhead_sampling
-from .unicode_breaks import validate_unicode_breaks
 
-__all__ = ('print_chars', 'overhead', 'overhead_sampling', 'validate_unicode_breaks')
+__all__ = ('print_chars', 'overhead', 'overhead_sampling')
