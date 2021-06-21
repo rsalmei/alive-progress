b9bd99ddba287d81dfaa4406879d0cfebe28e675
Rogério Sampaio <rsalmei@gmail.com>
2021-06-21 19:36:35 -0300

    fix tests after issue Flicker when printing with very slow throughput #83

--- tests/core/test_hook_manager.py
+++ tests/core/test_hook_manager.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+from threading import Condition
 from contextlib import contextmanager
 from unittest import mock
 
@@ -16,7 +17,7 @@ def hook(hook_manager):
 
 
 def test_hook_manager_captures_stdout(capsys):
-    hook_manager = buffered_hook_manager('nice {}! ', lambda: 35)
+    hook_manager = buffered_hook_manager('nice {}! ', lambda: 35, Condition())
     with hook(hook_manager):
         print('ok')
     assert capsys.readouterr().out == 'nice 35! ok\n'
@@ -29,28 +30,28 @@ def _hook_manager_captures_logging(capsys):
     logging.basicConfig(stream=sys.stderr)
     logger = logging.getLogger('?name?')
 
-    hook_manager = buffered_hook_manager('nice {}! ', lambda: 35)
+    hook_manager = buffered_hook_manager('nice {}! ', lambda: 35, Condition())
     with hook(hook_manager):
         logger.error('oops')
     assert capsys.readouterr().err == 'nice 35! ERROR:?name?:oops\n'
 
 
 def test_hook_manager_captures_multiple_lines(capsys):
-    hook_manager = buffered_hook_manager('nice {}! ', lambda: 35)
+    hook_manager = buffered_hook_manager('nice {}! ', lambda: 35, Condition())
     with hook(hook_manager):
         print('ok1\nok2')
     assert capsys.readouterr().out == 'nice 35! ok1\n         ok2\n'
 
 
 def test_hook_manager_can_be_disabled(capsys):
-    hook_manager = buffered_hook_manager('', None)
+    hook_manager = buffered_hook_manager('', None, Condition())
     with hook(hook_manager):
         print('ok')
     assert capsys.readouterr().out == 'ok\n'
 
 
 def test_hook_manager_flush(capsys):
-    hook_manager = buffered_hook_manager('', None)
+    hook_manager = buffered_hook_manager('', None, Condition())
     with hook(hook_manager):
         print('ok', end='')
         assert capsys.readouterr().out == ''
@@ -63,7 +64,7 @@ def test_hook_manager_flush(capsys):
 
 
 def test_hook_manager_do_clear_line_on_stdout():
-    hook_manager = buffered_hook_manager('', None)
+    hook_manager = buffered_hook_manager('', None, Condition())
     with hook(hook_manager), mock.patch('alive_progress.core.hook_manager.clear_line') as m_clear:
         print('some')
     m_clear.assert_called()
@@ -73,7 +74,7 @@ def test_hook_manager_do_not_flicker_screen_when_logging(capsys):
     logging.basicConfig()
     logger = logging.getLogger()
 
-    hook_manager = buffered_hook_manager('', None)
+    hook_manager = buffered_hook_manager('', None, Condition())
     with hook(hook_manager), mock.patch('alive_progress.core.hook_manager.clear_line') as m_clear:
         logger.error('oops')
     m_clear.assert_not_called()
@@ -90,14 +91,14 @@ def handlers():
 
 
 def test_install(handlers):
-    hook_manager = buffered_hook_manager('', None)
+    hook_manager = buffered_hook_manager('', None, Condition())
     with mock.patch('alive_progress.core.hook_manager._set_stream') as mock_set_stream:
         hook_manager.install()
     mock_set_stream.assert_has_calls(tuple(mock.call(h, mock.ANY) for h in handlers))
 
 
 def test_uninstall(handlers):
-    hook_manager = buffered_hook_manager('', None)
+    hook_manager = buffered_hook_manager('', None, Condition())
     with mock.patch('alive_progress.core.hook_manager._set_stream') as mock_set_stream:
         hook_manager.install()
         hook_manager.uninstall()
