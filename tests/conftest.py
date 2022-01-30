a3b44e82edcc1d3bfb2262746969da24c8e0c066
Rogério Sampaio <rsalmei@gmail.com>
2022-01-30 06:29:52 -0300

    improve spinner_test, to be able to test actual_length

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,13 +4,18 @@ import pytest
 @pytest.fixture
 def spinner_test():
     def spinner_test(*data):
-        def inner_factory(_=None):
+        def inner_factory(actual_length=None):
             def cycle_data():
                 while True:
                     yield from data
 
             def inner_spinner():
                 yield from next(cycle_gen)
+
+            actual_length = actual_length or inner_factory.natural
+            nonlocal data
+            ratio = int(actual_length / len(data[0][0])) + 1
+            data = tuple(tuple(tuple(d * ratio)[:actual_length] for d in cycle) for cycle in data)
 
             # generate spec info, just like the real one.
             frames = tuple(len(cycle) for cycle in data)
@@ -26,7 +31,7 @@ def spinner_test():
         # simulate to_cells().
         data = tuple(tuple(tuple(frame) for frame in cycle) for cycle in data)
 
-        inner_factory.natural = len(data[0])
+        inner_factory.natural = len(data[0][0])
         return inner_factory
 
     yield spinner_test
