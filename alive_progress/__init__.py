b3e236cdd86bae18a655d9826e86081ed69df768
Rogério Sampaio <rsalmei@gmail.com>
2025-11-28 02:41:57 -0300

    use metadata from pyproject

--- alive_progress/__init__.py
+++ alive_progress/__init__.py
@@ -1,12 +1,21 @@
+from importlib import metadata
+
 from .core.configuration import config_handler
 from .core.progress import alive_bar, alive_it
 
-VERSION = (3, 3, 0)
+try:
+    pkg_metadata = metadata.metadata('alive-progress')
+    __version__ = pkg_metadata['Version']
+    __author__ = pkg_metadata['Author-Email']
+    __email__ = __author__.split('<')[1][:-1]  # simple parser for "Name <email@addr.com>".
+    __description__ = pkg_metadata['Summary']  # also known as "description" in the metadata.
+except metadata.PackageNotFoundError:  # pragma: no cover
+    # the package is not installed, so we can't get the metadata; common during development.
+    __version__ = '0.0.0'
+    __author__ = None
+    __email__ = None
+    __description__ = None
 
-__author__ = 'Rogério Sampaio de Almeida'
-__email__ = 'rsalmei@gmail.com'
-__version__ = '.'.join(map(str, VERSION))
-__description__ = 'A new kind of Progress Bar, with real-time throughput, ' \
-                  'ETA, and very cool animations!'
+VERSION = tuple(map(int, __version__.split('.')))
 
 __all__ = ('alive_bar', 'alive_it', 'config_handler')
