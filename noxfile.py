33d6e0b046f8b2366261a78e9914c1bc0a83a34b
Rogério Sampaio <rsalmei@gmail.com>
2026-05-22 17:48:27 -0300

    remove Python 3.9 support (EOL), include Python 3.14

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 
-@nox.session(python=['3.9', '3.10', '3.11', '3.12', '3.13'])
+@nox.session(python=['3.10', '3.11', '3.12', '3.13', '3.14'])
 def tests(session):
     session.install('-r', 'requirements/test.txt', '-e', '.')
     session.run('pytest')
